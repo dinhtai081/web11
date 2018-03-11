81e6e31057e8210c133a2b0f1894c4ddd8e39715
dinhtai081 <dinhtai081@gmail.com>
2018-03-11 12:07:03 +0700

    BTVN lan 3

--- BTVNBuoi2/js-practices/handout/Practice3/generatePractice.js
+++ BTVNBuoi2/js-practices/handout/Practice3/generatePractice.js
@@ -1,36 +1,61 @@
 'use strict'
-function random(a,b){
-  return Math.floor((Math.random()* (b-a)) + a);
-}
-function sort(input){
-  return input.sort((a,b) => a-b);
-}
-function search(input,target){
-  return input.indexOf(target);
+function getRandomNumber(a,b){
+  return Math.floor(Math.random()*(b-a+1)) + a;
 }
 function generate(testLengthArray){
-    var x = new Array;
-    for (var i =0;i<testLengthArray.length ;i ++){
-      var y = new Array;
-      var len = testLengthArray[i];
-      while (len >0){
-        var xx = random(-1000,1000);
-        y.push(xx);
-        len--;
-      }
-      sort(y);
-      var t;
-      if (i==0) t=1001;
-      if (i==1) t=y[0];
-      if (i==2) t = y[y.length-1];
-      else t = x[random(1,y.length-1)];
-      x.push({
-        'input' : y,
-        'target' : t,
-        'output':search(y,t) 
-      });
+  var result = [];
+  testLengthArray.forEach((item, index) => {
+      var object = {};
+    object.input = [];
+    for(let i=0; i<item; i++){
+      object.input.push(getRandomNumber(-10000,10000));
     }
-    return x;
+    switch (index){
+      case 0:{
+        object.target = 10001;
+        break;
+      }
+      case 1:{
+        object.target = object.input[0];
+      }
+      case 2:{
+        object.target = object.input[item-1];
+      }
+      case 3:{
+        object.target = object.input[getRandomNumber(1,item -2)];
+        break;
+      }
+      default:{
+        object.target = object.input[getRandomNumber(0,item-1)];
+        break;
+      }
+    }
+    result.push(object);
+    }   
+  );
+  return result;
+    // var x = new Array;
+    // for (var i =0;i<testLengthArray.length ;i ++){
+    //   var y = new Array;
+    //   var len = testLengthArray[i];
+    //   while (len >0){
+    //     var xx = random(-1000,1000);
+    //     y.push(xx);
+    //     len--;
+    //   }
+    //   sort(y);
+    //   var t;
+    //   if (i==0) t=1001;
+    //   if (i==1) t=y[0];
+    //   if (i==2) t = y[y.length-1];
+    //   else t = x[random(1,y.length-1)];
+    //   x.push({
+    //     'input' : y,
+    //     'target' : t,
+    //     'output':search(y,t) 
+    //   });
+    // }
+    // return x;
   // return Array.from({length : testLengthArray.length})
   //   .map(item => ({
   //     input: Array.from({length: item}).map(item => []),
