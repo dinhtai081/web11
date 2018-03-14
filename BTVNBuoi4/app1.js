a181a1799f9a189a8cc96dcfd3bbb8205ba54c26
dinhtai081 <dinhtai081@gmail.com>
2018-03-14 19:13:30 +0700

    Up lai bt lan 4

--- BTVNBuoi4/app1.js
+++ BTVNBuoi4/app1.js
@@ -1,18 +1,18 @@
 const express = require("express");
 let app = express();
-app.use(express.static("public/intro"));
-//app.use(express.static("public/flexbox"));
+app.use(express.static("public/aintro"));
+app.use(express.static("public/flexbox"));
 
 app.use(express.static("public/frontendpractice"));
 app.get('/',(req, res)=>{
-    res.sendFile(__dirname + "/public/intro/btvn1.html");
+    res.sendFile(__dirname + "/public/aintro/index.html");
 });
  app.get('/frontendpractice', (req,res) =>{
      res.sendFile(__dirname + "/public/frontend/index.html");
 });
-// app.get('/flexbox', (req,res) =>{
-//     res.sendFile(__dirname + "/public/flexbox/index.html");
-// });
+app.get('/flexbox', (req,res) =>{
+    res.sendFile(__dirname + "/public/flexbox/index.html");
+});
 app.listen(6001 ,(err)=>{
     if (err) {console.log(err);}
     console.log("App start at port 6001!");
